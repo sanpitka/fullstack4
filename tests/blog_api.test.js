42c542d4ce3f155f42fa21016527e1ce1b35739b
sanpitka <sanpitka@student.oulu.fi>
2024-10-29 22:06:48 +0200

    4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after } = require('node:test')
+const { test, describe, after } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -7,16 +7,24 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe.only('number of blogs', () => {
+  test.only('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('there are right amount of blogs', async () => {
-  const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  test.only('there are right amount of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test.only('The unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    console.log(response.body)
+    assert(response.body[0].id)
+  })
 })
 
 after(async () => {
